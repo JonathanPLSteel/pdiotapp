c7b20605016336d0b71c75751367f1c104cabd1b
Jonathan-Steel <j.p.l.steel@sms.ed.ac.uk>
2024-11-13 16:31:38 +0000

    Respiratory model works pretty well!

--- app/src/main/java/com/specknet/pdiotapp/live/ClassifyActivity.kt
+++ app/src/main/java/com/specknet/pdiotapp/live/ClassifyActivity.kt
@@ -11,7 +11,6 @@ import android.os.Looper
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.TextView
-import android.widget.Button
 import com.specknet.pdiotapp.R
 import com.specknet.pdiotapp.utils.Constants
 import com.specknet.pdiotapp.utils.RESpeckLiveData
@@ -25,9 +24,10 @@ import org.json.JSONArray
 class ClassifyActivity : AppCompatActivity() {
     private val TAG = "ClassifyingActivity"
     // Define variables for the model and live data here
-    private lateinit var resultTextView: TextView
-    private lateinit var classifyButton: Button
-    private lateinit var tflite: Interpreter
+    private lateinit var activityResultTextView: TextView
+    private lateinit var respiratoryResultTextView: TextView
+    private lateinit var activityClassifier: Interpreter
+    private lateinit var respiratoryClassifier: Interpreter
 
     // global broadcast receiver so we can unregister it
     lateinit var respeckReceiver: BroadcastReceiver
@@ -42,41 +42,54 @@ class ClassifyActivity : AppCompatActivity() {
     val filterTestRespeck = IntentFilter(Constants.ACTION_RESPECK_LIVE_BROADCAST)
     val filterTestThingy = IntentFilter(Constants.ACTION_THINGY_BROADCAST)
 
-    private val classes = mapOf(
-        0 to "ascending_stairs",
-        1 to "descending_stairs",
-        2 to "lying_back",
-        3 to "lying_left",
-        4 to "lying_right",
-        5 to "lying_stomach",
-        6 to "misc_movement",
-        7 to "normal_walking",
+    private val activityClasses = mapOf(
+        0 to "ascending stairs",
+        1 to "descending stairs",
+        2 to "lying on back",
+        3 to "lying on left side",
+        4 to "lying on right side",
+        5 to "lying on stomach",
+        6 to "miscellaneous movement",
+        7 to "normal walking",
         8 to "running",
-        9 to "shuffle_walking",
-        10 to "sitting_standing"
+        9 to "shuffle walking",
+        10 to "sitting/standing"
     )
 
-    private lateinit var slidingWindowBuffer: MutableList<FloatArray>;
+    private val respiratoryClasses = mapOf(
+        0 to "coughing",
+        1 to "hyperventilating",
+        2 to "breathing normally",
+        3 to "other respiratory condition",
+    )
+
+    private val stationaryClasses = arrayOf(2,3,4,5,10);
+
+    private lateinit var activityWindowBuffer: MutableList<FloatArray>;
+    private lateinit var respiratoryWindowBuffer: MutableList<FloatArray>;
     private val windowSize = 50;
-    private val activityFeatures = 6;
 
     private var latestRespeckData: FloatArray? = null;
     private var latestThingyData: FloatArray? = null;
+
+    private var isClassifying = false;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_classify)
 
         // Initialize views
-        resultTextView = findViewById(R.id.result_text_view)
-        classifyButton = findViewById(R.id.classify_button)
+        activityResultTextView = findViewById(R.id.activity_result_text_view)
+        respiratoryResultTextView = findViewById(R.id.respiratory_result_text_view)
         respeckAccel = findViewById(R.id.respeck_accel)
         respeckWindows = findViewById(R.id.respeck_windows)
         thingyAccel = findViewById(R.id.thingy_accel)
 
-        tflite = Interpreter(loadModelFile());
+        activityClassifier = Interpreter(loadModelFile("physical-activity-model.tflite"));
+        respiratoryClassifier = Interpreter(loadModelFile("respiratory-model.tflite"));
 
-        slidingWindowBuffer = mutableListOf();
+        activityWindowBuffer = mutableListOf();
+        respiratoryWindowBuffer = mutableListOf();
 
         respeckReceiver = object : BroadcastReceiver() {
             override fun onReceive(context: Context, intent: Intent) {
@@ -130,19 +143,16 @@ class ClassifyActivity : AppCompatActivity() {
         updateWindow(respeckData, null);
 
         val output = "[" + liveData.accelX.toString() + "," + liveData.accelY + "," + liveData.accelZ + "]"
-        Log.d(TAG, "updateRespeckData: appended to respeckoutputdata = " + output)
-        // update UI thread
         runOnUiThread {
             respeckAccel.text = getString(R.string.respeck_accel, liveData.accelX, liveData.accelY, liveData.accelZ)
         }
     }
+
     private fun updateThingyData(liveData: ThingyLiveData) {
         val thingyData = floatArrayOf(liveData.accelX, liveData.accelY, liveData.accelZ);
         updateWindow(null, thingyData);
 
         val output = "[" + liveData.accelX.toString() + "," + liveData.accelY + "," + liveData.accelZ + "]"
-        Log.d(TAG, "updateThingyData: appended to thingyOutputData = " + output)
-        // update UI thread
         runOnUiThread {
             thingyAccel.text = getString(R.string.thingy_accel, liveData.accelX, liveData.accelY, liveData.accelZ)
         }
@@ -158,16 +168,25 @@ class ClassifyActivity : AppCompatActivity() {
 
         if (latestRespeckData != null && latestThingyData != null) {
             val combinedData = latestRespeckData!! + latestThingyData!!
-            slidingWindowBuffer.add(combinedData)
+            activityWindowBuffer.add(combinedData)
+
+            respiratoryWindowBuffer.add(latestRespeckData!!)
 
             // Remove oldest data if buffer exceeds the window size
-            if (slidingWindowBuffer.size > windowSize) {
-                slidingWindowBuffer.removeAt(0)
+            if (activityWindowBuffer.size > windowSize) {
+                activityWindowBuffer.removeAt(0)
+            }
+
+            // Remove oldest data if buffer exceeds the window size
+            if (respiratoryWindowBuffer.size > windowSize) {
+                respiratoryWindowBuffer.removeAt(0)
             }
 
             // Run classification if buffer has enough data
-            if (slidingWindowBuffer.size == windowSize) {
+            if (activityWindowBuffer.size == windowSize) {
+                isClassifying = true;
                 classifyActivity();
+                isClassifying = false
             }
 
             // Reset the latest data for the next round
@@ -177,26 +196,56 @@ class ClassifyActivity : AppCompatActivity() {
     }
 
     private fun classifyActivity() {
-        val inputArray = arrayOf(slidingWindowBuffer.toTypedArray());
-        val outputArray = Array(1) {FloatArray(classes.size)}
 
-        tflite.run(inputArray, outputArray);
-
-        val predictedClassIndex = outputArray[0].indices.maxByOrNull { outputArray[0][it] };
-        val predictedClass = classes[predictedClassIndex];
-
-        runOnUiThread {
-            resultTextView.text = predictedClass ?: "Unknown Activity"
-            Log.d(TAG, "Predicted Activity: $predictedClass")
+        if (activityWindowBuffer.size < windowSize || respiratoryWindowBuffer.size < windowSize) {
+            Log.e(TAG, "Buffers do not have enough data for classification. Skipping.")
+            return
         }
 
-        slidingWindowBuffer.clear();
+        val activityInputArray = arrayOf(activityWindowBuffer.toTypedArray());
+        val activityOutputArray = Array(1) {FloatArray(activityClasses.size)}
+
+        activityClassifier.run(activityInputArray, activityOutputArray);
+
+        val predictedActivityClassIndex = activityOutputArray[0].indices.maxByOrNull { activityOutputArray[0][it] };
+        val predictedActivity = activityClasses[predictedActivityClassIndex];
+
+        Log.d(TAG, "Predicted Activity: $predictedActivity")
+
+        var predictedRespiratory = "N/A"
+
+        if (predictedActivityClassIndex in stationaryClasses) {
+            Log.d(TAG, "Stationary activity so attempting to predict respiratory...")
+
+            val respiratoryInputArray = arrayOf(respiratoryWindowBuffer.toTypedArray());
+            Log.d(TAG, "Input Array Shape: ${respiratoryInputArray.size},${respiratoryInputArray[0].size},${respiratoryInputArray[0][0].size}")
+
+            val respiratoryOutputArray = Array(1) {FloatArray(respiratoryClasses.size)}
+            Log.d(TAG, "Output Array Shape: ${respiratoryOutputArray.size},${respiratoryOutputArray[0].size}")
+
+            respiratoryClassifier.run(respiratoryInputArray, respiratoryOutputArray)
+
+            val predictedRespiratoryClassIndex = respiratoryOutputArray[0].indices.maxByOrNull { respiratoryOutputArray[0][it] };
+            predictedRespiratory = respiratoryClasses[predictedRespiratoryClassIndex]!!;
+        }
+
+        Log.d(TAG, "Predicted Respiratory: $predictedRespiratory")
+
+        runOnUiThread {
+            activityResultTextView.text = predictedActivity ?: "Unknown Activity"
+            respiratoryResultTextView.text = predictedRespiratory
+        }
+
+        activityWindowBuffer.clear()
+        respiratoryWindowBuffer.clear()
     }
 
     // Load the TensorFlow Lite model from the assets directory as a MappedByteBuffer
-    private fun loadModelFile(): MappedByteBuffer {
+    private fun loadModelFile(filename: String): MappedByteBuffer {
         // Open the file descriptor for the model file in the assets folder
-        val fileDescriptor = assets.openFd("physical-activity-model.tflite")
+        val fileDescriptor = assets.openFd(filename)
+
+        Log.d(TAG, "Loading model file: $filename, Size: ${fileDescriptor.declaredLength}")
 
         // Create a FileInputStream to read the file using the file descriptor
         val inputStream = FileInputStream(fileDescriptor.fileDescriptor)
@@ -243,7 +292,7 @@ class ClassifyActivity : AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
 
-        // Release model resources if needed
-        tflite.close();
+        activityClassifier.close();
+        respiratoryClassifier.close();
     }
 }
